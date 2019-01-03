a1108433f93444e66b286be013db98fb6b22eb26
arseniy <apopov@sellbery.com>
2019-01-03 15:14:06 +0300

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/alise', function (req, res) {
+  res.send({ response: "done" , req: ...req.query });
+});
+app.listen(8400, function () {
+  console.log('Example app listening on port 8400!');
+});
